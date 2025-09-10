b36ff4edbae9ca7d5105fa2fb30dc8837bfd2d58
Martin Albarracin <mafranzone@untref.edu.ar>
2025-09-10 18:48:49 -0300

    Se arregla diccionario

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -17,3 +17,36 @@ func NewDictionary[K comparable, V any](hashFunc func(key K) uint) *Dictionary[K
 func (d *Dictionary[K, V]) Set(key K, value V) {
 	d.hash.Put(key, value)
 }
+
+func (d *Dictionary[K, V]) Get(key K) V {
+	return d.hash.Get(key)
+}
+
+func (d *Dictionary[K, V]) Size() int {
+	return d.hash.Size()
+}
+
+func (d *Dictionary[K, V]) Remove(key K) {
+	d.hash.Remove(key)
+}
+
+func (d *Dictionary[K, V]) Contains(key K) bool {
+	return d.hash.Contains(key)
+}
+
+func (d *Dictionary[K, V]) Keys() []K {
+	return d.hash.Keys()
+}
+
+func (d *Dictionary[K, V]) Values() []V {
+	return d.hash.Values()
+}
+
+func (d *Dictionary[K, V]) IsEmpty() bool {
+	return d.hash.IsEmpty()
+}
+
+func (d *Dictionary[K, V]) Clear() {
+	d.hash.Clear()
+}
+
