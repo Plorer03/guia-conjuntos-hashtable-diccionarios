40d396d12cd1b6a18e99eb7aca13e601b46c1440
Lucas Videla <videla.lucas@gmail.com>
2025-09-11 08:45:41 -0300

    Fixed: se referencia al dictionary de la propia guia, en lugar de data-structures

--- ejercicios/diccionarios.go
+++ ejercicios/diccionarios.go
@@ -1,7 +1,7 @@
 package ejercicios
 
 import (
-	"github.com/untref-ayp2/data-structures/dictionary"
+	"untref-ayp2/guia-conjuntos-hashes-diccionarios/dictionary"
 	"untref-ayp2/guia-conjuntos-hashes-diccionarios/list"
 )
 
